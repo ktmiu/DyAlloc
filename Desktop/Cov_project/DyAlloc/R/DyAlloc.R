01904ce98bffbf0759dd040a309c7735ba94e9b9
Katie Liu <katieliu.oboe@gmail.com>
2021-10-20 18:15:36 -0400

    updated R and Description files and added reference manual

--- Desktop/Cov_project/DyAlloc/R/DyAlloc.R
+++ Desktop/Cov_project/DyAlloc/R/DyAlloc.R
@@ -1,7 +1,10 @@
-#' Creates a marginal imbalance score for each treatment arm of N treatment groups
+#' Creates a marginal imbalance score for each treatment arm of N treatment groups.
+#' This is a low level function to be used in subsequent functions and will most
+#' likely not be used directly by a practitioner.
 #' @export
-#' @param df data frame
-#' @param N numeric variable
+#' @param df data frame. The profiles of existing subjects in the study.
+#' @param N numeric variable. The number of treatment arms in the study.
+#' @return A vector of marginal imbalance scores for each treatment arm is produced (a total of N scores)
 MarImb <- function(df,N) {
   #INPUT: a data frame and number of treatment arms
   #OUTPUT: marginal imbalance score for each treatment arm (a total of N scores)
@@ -34,13 +37,18 @@ MarImb <- function(df,N) {
 }
 
 #' Creates a weighted imbalance score for each treatment arm
+#' *NOTE: the default for site here is FALSE, but one can specify site=TRUE in the argument
+#' to account for site imbalance. Note that the weight for site has to be placed in the
+#' third element of the 'weight' vector (and can be omitted when site=FALSE)*
 #' @export
-#' @param df data frame
-#' @param N numeric variable
-#' @param covars a vector
-#' @param weight a vector
-#' @param newsub a vector
-#' @param site a boolean value
+#' @param df data frame.The profiles of existing subjects in the study.
+#' @param N numeric variable. The number of treatment arms in the study.
+#' @param covars a vector. All the names of covariates in the study.
+#' @param weight a vector. The weights for overall study, with-in stratum, (site),
+#' and factors/covariates, in this particular order
+#' @param newsub a vector. The factor profile of the new subject entering the study.
+#' @param site a boolean value. Whether or not to account for site imbalance (default:FALSE)
+#' @return A vector of weighted imbalance scores for each treatment arm is produced (a total of N scores)
 
 WeiImb <- function(df,N,covars,weight,newsub,site=FALSE) {
   ##INPUT: df = data frame, covars = a vector of the name of covariates in the study,
@@ -106,10 +114,18 @@ WeiImb <- function(df,N,covars,weight,newsub,site=FALSE) {
   return(wimb)
 }
 
-#' Creates a vector of probabilities of being assigned to each treatment group
+#' Creates a vector of probabilities of being assigned to each treatment group.
+#' This function uses the imbalance scores created from the function WeiImb as input.
+#' *NOTE: if the imbalance scores are the same, then they have equal probability of
+#' being assigned to. E.g. if we get two equal least weighted imbalance scores, they each
+#' have a probability of 0.5 for a subject to be assigned to.*
 #' @export
-#' @param imbalances a vector
-#' @param alpha a numeric number
+#' @param imbalances a vector. Imbalance scores across the N treatment groups
+#' (generally, the scores we get from WeiImb would be plugged in here)
+#' @param alpha a numeric number. The "second best probability" parameter, generally alpha is set to 0.2.
+#' This means that the treat group a subject assign to gets the probability of 0.8.
+#' @return A vector of probabilities of being assigned to each treatment group.
+
 Trt_Prob <- function(imbalances,alpha){
   #INPUT: the imbalance scores across treatment groups and the "second best probability" parameter
   #OUTPUT: a vector of probabilities of being assigned to each treatment group
@@ -139,14 +155,19 @@ Trt_Prob <- function(imbalances,alpha){
   return(prob)
 }
 
-#' provides the final treatment group assignment
+#' Provides the final treatment group allocation for a new subject entering the study.
+#'  Incorporates all the previous functions and a practitioner can use this function solely
+#'  to obtain treatment assignments for the study subjects. The site default is FALSE.
 #' @export
-#' @param df data frame
-#' @param N numeric variable
-#' @param covars a vector
-#' @param weight a vector
-#' @param newsub a vector
-#' @param site a boolean value
+#' @param df data frame. The profiles of existing subjects in the study.
+#' @param N numeric variable. The number of treatment arms
+#' @param covars a vector. All the names of covariates in the study.
+#' @param weight a vector. The weights for overall study, with-in stratum, (site),
+#' and factors/covariates, in this particular order
+#' @param newsub a vector. The factor profile of the new subject entering the study.
+#' @param site a boolean value. Whether or not to account for site imbalance (default:FALSE)
+#' @return A treatment group allocation
+
 DyAlloc <- function(df, N, covars, weight, newsub, site=FALSE) {
   imb <- WeiImb(df, N, covars, weight, newsub, site)
   prob <- Trt_Prob(imb,0.2)
